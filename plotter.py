99a9f697c2df0bfa6a58075314303951683d8594
Rigel Madraswalla <rigel_madraswalla@epam-continuum.com>
2019-02-07 17:44:41 -0500

    Works to mass process outfiles

--- plotter.py
+++ plotter.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import plotly.offline as py
+import plotly.graph_objs as go
 import numpy as np
-import csv, re, sys
+import csv, re, sys, os
+import plotly.io as pio
 
 def parse_file(name, number):
 
@@ -33,12 +36,13 @@ def parse_file(name, number):
         reading_nums = []
         loads = []
         travels = []
-        times = []
         for row in csvreader:
-            reading_nums.append(int(row['Reading']))
-            loads.append(float(row['Load']))
-            travels.append(float(row['Travel']))
-            times.append(float(row['Time']))
+            try:
+                reading_nums.append(int(row['Reading']))
+                loads.append(float(row['Load']) if 'Load' in row else float(row['Load [N]']))
+                travels.append(float(row['Travel']) if 'Travel' in row else float(row['Travel [mm]']))
+            except ValueError as e:
+                continue
 
         num_readings = reading_nums[-1] # Total number of readings
         total_travel_dist = -1 * travels[-1] # Should be -2 for most
@@ -77,17 +81,19 @@ def plot_test(name):
     x_axes = []
     y_axes = []
 
+    regex = r"(\d+\_\d+)_(\D+)_(\d+)"
+    spl = re.search(regex, name)
+    grip_length = spl.group(1)
+    grip_type = spl.group(2)
+    tube_num = spl.group(3)
+
+    first_dir = grip_length + "_" + grip_type
+    second_dir = grip_type + "_" + tube_num
+
     test = 0
     for i in range(1, 50):
         try:
-            regex = r"(\d+\_\d+)_(\D+)_(\d+)"
-            spl = re.search(regex, name)
-            grip_length = spl.group(1)
-            grip_type = spl.group(2)
-            tube_num = spl.group(3)
 
-            first_dir = grip_length + "_" + grip_type
-            second_dir = grip_type + "_" + tube_num
             filename = "csvs/" + first_dir + "/" + second_dir + "/" + name + "_" + str(i) + ".log"
             ret = parse_file(filename, i)
             numbers.append(ret[0])
@@ -95,7 +101,6 @@ def plot_test(name):
             y_axes.append(ret[2])
             test += 1
         except FileNotFoundError as e:
-            #print (e)
             pass
 
     index_hi = numbers.index(highest_avg[1])
@@ -107,6 +112,7 @@ def plot_test(name):
     y_axes.append(y_axes[index_hi])
     y_axes.append(y_axes[index_lo])
 
+    data = []
     for i in range(0, len(numbers)):
         if numbers[i] == highest_avg[1]:
             color = "#FFC000"
@@ -117,16 +123,65 @@ def plot_test(name):
         else:
             color = "#dbdad6" # Dark gray
             linewidth = 1.0
-        plt.plot(x_axes[i], y_axes[i], linewidth=linewidth, label=str(numbers[i]), color=color)
+        data.append (
+            go.Scatter(
+                x = x_axes[i],
+                y = y_axes[i],
+                name = numbers[i],
+                line = dict (
+                    color = color,
+                    width = linewidth
+                ),
+                showlegend=False
+            )
+        )
+
+    title = ""
+    title += grip_length.replace("_", ".") + "mm "
+    if grip_type == "S":
+        title += "Smooth "
+    elif grip_type == "T":
+        title += "Textured "
+
+    if tube_num == "1":
+        title += "Large PP"
+    elif tube_num == "2":
+        title += "Large PS"
+    elif tube_num == "3":
+        title += "Small PS"
+    elif tube_num == "4":
+        title += "Small PP"
+    elif tube_num == "5":
+        title += "Small PTE"
+    elif tube_num == "6":
+        title += "Large PTE"
+
+    # Edit the layout
+    layout = dict(title = title,
+                  xaxis = dict(title = "Travel (mm)"),
+                  yaxis = dict(title = "Load (N)"),
+                  )
+
+    fig = dict(data=data, layout=layout)
+    fileout = "out/" + first_dir + "/" + second_dir + "/" + name
+    py.plot(fig, filename=fileout + ".html", auto_open=False)
+    pio.write_image(fig, fileout + ".png")
 
     print ("Found ", test, " tests")
 
 if __name__ == "__main__":
-    name = "2_5_S_2"
-    plot_test(name)
-
-    plt.title(name)
-    plt.xlabel("Travel (mm)")
-    plt.ylabel("Load (N)")
-
-    plt.show()
+    runs = [
+        "2_5_S",
+        "2_5_T",
+        "4_0_S",
+        "4_0_T",
+        "5_5_S",
+        "5_5_T"
+    ]
+    for each in runs:
+        for i in range(1,7):
+            name = each + "_" + str(i)
+            try:
+                plot_test(name)
+            except Exception as e:
+                print("Error on", name, ":", e.__traceback__)
